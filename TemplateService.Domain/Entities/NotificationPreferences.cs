dc7401b88e41463fe1a6626b075add3de84ebe0b
SkyReed <itzskyreed@gmail.com>
2025-06-24 20:42:38 +0500

    Добавлены Entity пользователя, и его предпочтений

---
+++ TemplateService.Domain/Entities/NotificationPreferences.cs
@@ -0,0 +1,32 @@
+﻿namespace TemplateService.Domain.Entities;
+
+/// <summary>
+/// Настройки уведомлений пользователя
+/// </summary>
+public class NotificationPreferencesEntity
+{
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Уведомлять в Telegram
+    /// </summary>
+    public bool NotifyTelegram { get; set; }
+
+    /// <summary>
+    /// Уведомлять по Email
+    /// </summary>
+    public bool NotifyEmail { get; set; }
+
+    /// <summary>
+    /// Напомнить за 1 день
+    /// </summary>
+    public bool ReminderBefore1Day { get; set; }
+
+    /// <summary>
+    /// Напомнить за 1 час
+    /// </summary>
+    public bool ReminderBefore1Hour { get; set; }
+}
---
+++ TemplateService.Domain/Entities/UserEntity.cs
@@ -0,0 +1,52 @@
+﻿namespace TemplateService.Domain.Entities;
+
+/// <summary>
+/// Пользователь
+/// </summary>
+public class UserEntity
+{
+    /// <summary>
+    /// Идентификатор пользователя
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Логин
+    /// </summary>
+    public string Login { get; set; }
+
+    /// <summary>
+    /// Хэш пароля
+    /// </summary>
+    public string PasswordHash { get; set; }
+
+    /// <summary>
+    /// Имя
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Фамилия
+    /// </summary>
+    public string Surname { get; set; }
+
+    /// <summary>
+    /// Электронная почта (может быть null)
+    /// </summary>
+    public string? Email { get; set; }
+
+    /// <summary>
+    /// Telegram ID (может быть null)
+    /// </summary>
+    public string? TelegramId { get; set; }
+
+    /// <summary>
+    /// ID настроек уведомлений (внешний ключ)
+    /// </summary>
+    public int NotificationPreferencesId { get; set; }
+
+    /// <summary>
+    /// Навигационное свойство на настройки уведомлений
+    /// </summary>
+    public virtual NotificationPreferencesEntity NotificationPreferences { get; set; }
+}
