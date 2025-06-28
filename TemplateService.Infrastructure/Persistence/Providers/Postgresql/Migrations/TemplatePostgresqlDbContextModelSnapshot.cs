0c0830cf18fed985af7edb167fc8b788515d0a09
ItzSkyReed <itzskyreed@gmail.com>
2025-06-28 16:31:24 +0500

    feat(db): Автоматически применяем миграции при старте

--- TemplateService.Infrastructure/Persistence/Providers/Postgresql/Migrations/TemplatePostgresqlDbContextModelSnapshot.cs
+++ TemplateService.Infrastructure/Persistence/Providers/Postgresql/Migrations/TemplatePostgresqlDbContextModelSnapshot.cs
@@ -2,21 +2,18 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
-using TemplateService.Infrastructure.Persistence;
+using TemplateService.Infrastructure.Persistence.Providers.Postgresql;
 
 #nullable disable
 
 namespace TemplateService.Infrastructure.Persistence.Providers.Postgresql.Migrations
 {
-    [DbContext(typeof(TemplateDbContext))]
-    [Migration("20250628105812_Initial")]
-    partial class Initial
+    [DbContext(typeof(TemplatePostgresqlDbContext))]
+    partial class TemplatePostgresqlDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TemplateService.Infrastructure/Persistence/Providers/Postgresql/TemplatePostgresqlDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace TemplateService.Infrastructure.Persistence.Providers.Postgresql;
+
+public class TemplatePostgresqlDbContextFactory : IDesignTimeDbContextFactory<TemplatePostgresqlDbContext>
+{
+    public TemplatePostgresqlDbContext CreateDbContext(string[] args)
+    {
+        // Чтение конфигурации вручную
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("tmp-appsettings.json", optional: false)
+            .AddJsonFile("tmp-appsettings.Development.json", optional: true)
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        // Построение DataSource с dynamic json
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+        dataSourceBuilder.EnableDynamicJson();
+        var dataSource = dataSourceBuilder.Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<TemplatePostgresqlDbContext>();
+        optionsBuilder.UseNpgsql(dataSource);
+
+        return new TemplatePostgresqlDbContext(optionsBuilder.Options, configuration);
+    }
+}
