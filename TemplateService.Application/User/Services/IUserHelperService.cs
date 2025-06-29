3f05b5c4133d70dbed6e72e3a563dae4c9643c52
SkyReed <itzskyreed@gmail.com>
2025-06-29 20:49:33 +0500

    feat: в UserDto добавлена путь до аватара пользователя

---
+++ TemplateService.Application/User/Services/IUserHelperService.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using TemplateService.Application.User.DTOs;
+using TemplateService.Domain.Entities;
+
+namespace TemplateService.Application.User.Services;
+
+public interface IUserHelperService
+{
+    string GetAvatarUrl(Guid userId);
+    bool IsAvatarExists(byte[]? avatar);
+    UserDto BuildUserDto(UserEntity user, IMapper mapper);
+}
